ee291bc2842b421ecb3719aa2ca722586d4cf13e
EdgarHan <poseidon0288@gmail.com>
2018-09-30 20:29:57 +0900

    reddux-thunk , axios 까지 적용한부분

--- 5redux-starter-kit/README.md
+++ 5redux-starter-kit/README.md
@@ -7,3 +7,16 @@
 ---------------
 yarn add redux-logger 
 적용 방법은 sotre.js 확인, 원리는 loggerMiddleware.js 파일 확인할것
+
+# redux-thunk
+thunk란 특정 작업을 미루려고 함수형태로 감싼것을 의미한다
+ex) const x = 1+2 는 바로실행되지만
+context x = () => 1+2; 는 x를 호출해야 실행된다.
+redux에서 action을 dispatch 할때 함수형태로 dispatch 할수있게 해준다.
+이는 특정 action 을 시간차 또는 if문 분기를 태워 실행시킬수 있음을의미한다. middle ware는 action을 reducer로 보낼떄 중간에 후킹해서 추가작업을 해주는것이다.
+
+# redux-promise-middleware
+
+Promise 객체를 payload로 전달하면서 시작, 성공, 실패할때 액션뒷부분에 _PENDING, _FULFILLED< _REJECTED를 붙여서 반환합니다.
+각 그렇게된다면 액션타입을 굳이 3종류로 나눠서 설정할필요가없죠.,
+대신 우리는 기존코드에서 SUCCESS FAILUER를 사용하니 이를 임의값으로 설정하겠습니다.
--- 5redux-starter-kit/src/App.js
+++ 5redux-starter-kit/src/App.js
@@ -2,16 +2,56 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
-
+import post, * as postActions from './modules/post';
+import axios from 'axios';
 
 class App extends Component {
+
+    loadData = () => {
+        const { PostActions, number } = this.props;
+        PostActions.getPost(number).then(
+            (response) => {
+                console.log(response);
+            }
+        ).catch(
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // 이전 number와 현재 number 가 다르면 요청을 시작합니다.
+        if(this.props.number !== prevProps.number) {
+            this.loadData();
+        }
+    }
+
     render() {
-        const { CounterActions, number } = this.props;
+        const { CounterActions, number, post, error, loading } = this.props;
 
         
         return (
             <div>
                 <h1>{number}</h1>
+                {
+                    loading 
+                    ? (<h2>로딩중....</h2>)
+                    : (
+                        error
+                        ? (<h2>오류 발생!</h2>)
+                        : (
+                            <div>
+                                <h2>{post.title}</h2>
+                                <p>{post.body}</p>
+                            </div>
+                        )
+                    )
+                }
                 <button onClick={CounterActions.increment}>+</button>
                 <button onClick={CounterActions.decrement}>-</button>
             </div>
@@ -21,9 +61,13 @@ class App extends Component {
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        post: state.post.data,
+        loading: state.post.pending,
+        error: state.post.error
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions: bindActionCreators(postActions, dispatch)
     })
 )(App);
