1b11f2f70dfd779655afc00b7572cd7eeee12f09
EdgarHan <poseidon0288@gmail.com>
2018-10-01 11:09:27 +0900

    라우터 완료

--- 6react-router-tutorial/src/App.js
+++ 6react-router-tutorial/src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import {
   Home,
-  About
+  About,
+  Posts
 } from 'pages';
+import Menu from 'components/Menu';
 
 const App = () => {
   return (
     <div>
       {/* Root 경로에는 무조건 Exact가 들어가야합니다. */}
+      <Menu/>
       <Route exact path="/" component={Home}/>
       <Route path="/about/:name?" component={About}/>
+      <Route path="/posts" component={Posts}/>
     </div>
   );
 };
